0b31f09e65284299ca6162cf61291396c312d778
Mateusz <bednarczyk.matt@gmail.com>
2023-11-21 20:12:29 +0100

    Exception handling for posting null/empty Match field

--- src/main/java/com/sbs/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/sbs/controllers/ControllerExceptionHandler.java
@@ -3,17 +3,35 @@ package com.sbs.controllers;
 import com.sbs.utils.Exceptions.CouponInPlayException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-    @ExceptionHandler (CouponInPlayException.class)
+    @ExceptionHandler(CouponInPlayException.class)
     public ResponseEntity<Map<String, String>> handleCouponInPlayException(CouponInPlayException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("Error", "status 400, Coupon in play cannot be edited"));
+    }
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("status", "400", "errors", prepareMapFromValidationErrors(ex)));
+    }
+    private String prepareMapFromValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errorMap.put(fieldName, errorMessage);
+        });
+        return errorMap.toString();
     }
 }
